b9b671b131b342cdaaebc54b16fadc658176bdcc
Miguel <mmirandasanchez16@gmail.com>
2024-11-12 14:16:40 -0300

    script added

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const getWeather = async(cityName) => {
-    const apikey = e57f27c6abed63d3cd7645cd175d2d2a
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`
+    const apikey = "e57f27c6abed63d3cd7645cd175d2d2a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`;
 
     try {
-        const data = await fetchWeather(url);
-        if( data.status !== 200) console.log('there was an error')
+        const data = await fetch(url);
+        const weatherData = await data.json();
+        if(weatherData?.code === '404') return console.log('there was an error')
+        console.log(weatherData)
 
-        console.log(data)
     } catch (error) {
-        console.log('there was an error')
+        console.log("An Error Ocurred => " + error.message);
+    }
+
+    return {
+
     }
 }
-
 
 const form = document.querySelector("#inputCity");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault;
-    const getCityName = e.target.city.value;
-    console.log(getCityName);
-    alert("aler")
+    e.preventDefault();
+    const getCityName = e.target.querySelector(".nav input[name='city']").value;
+    const {} = getWeather(getCityName);
+
 }); 
